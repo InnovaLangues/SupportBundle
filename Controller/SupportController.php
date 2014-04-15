a22a8c69274123315c5725a2778fe8d7dc921494
Elorfin <penin.axel@gmail.com>
2014-04-15 16:48:48 +0200

    make plugin works

--- Controller/SupportController.php
+++ Controller/SupportController.php
@@ -2,6 +2,10 @@
 
 namespace Innova\SupportBundle\Controller;
 
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+
 /**
  * Class SupportController
  *
--- InnovaSupportBundle.php
+++ InnovaSupportBundle.php
@@ -4,12 +4,14 @@ namespace Innova\SupportBundle;
 
 use Claroline\CoreBundle\Library\PluginBundle;
 use Claroline\KernelBundle\Bundle\AutoConfigurableInterface;
+use Claroline\KernelBundle\Bundle\ConfigurationProviderInterface;
 use Claroline\KernelBundle\Bundle\ConfigurationBuilder;
+use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 /**
  * Bundle class.
  */
-class InnovaSupportBundle extends PluginBundle implements AutoConfigurableInterface
+class InnovaSupportBundle extends PluginBundle implements AutoConfigurableInterface, ConfigurationProviderInterface
 {
     public function supports($environment)
     {
@@ -21,5 +23,15 @@ class InnovaSupportBundle extends PluginBundle implements AutoConfigurableInterf
         $config = new ConfigurationBuilder();
 
         return $config->addRoutingResource(__DIR__ . '/Resources/config/routing.yml', null, 'innova_support');
+    }
+
+    public function hasMigrations()
+    {
+        return false;
+    }
+
+    public function suggestConfigurationFor(Bundle $bundle, $environment)
+    {
+
     }
 }
